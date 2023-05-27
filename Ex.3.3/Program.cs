5e7bc3367a5f7570ad34f1da3ed74ee5eb22987d
Hoàng Hữu Tiến Đạt <dathht.21ad@vku.udn.vn>
2023-05-27 16:31:14 +0700

    ex

---
+++ Ex.3.3/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Ex3.3 - Ứng dụng đọc số thành chữ
+using System;
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập số cần đọc: ");
+            int n = int.Parse(Console.ReadLine());
+            string[] num = new string[10] {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+            string[] num2 = new string[]{"eleven", "twelve", "thirteen", "fourteen", "fifteen"};
+            if (n>=0 &&n < 10)
+            {
+                Console.WriteLine(num[n]);
+            }
+            if (n>=11 && n<16)
+            {
+               Console.WriteLine(num2[n-11]);
+            }
+            if (n>=16 && n<20)
+            {
+                Console.WriteLine(num[n-10]+"teen");
+            }
+            if (n>=20 && n<100)
+            {
+                Console.WriteLine(num[n/10]+"ty "+num[n%10]);
+            }
+            if (n>=100 && n<1000)
+            {
+                Console.WriteLine(num[n/100]+" hundred "+num[(n%100)/10]+"ty "+num[n%10]);
+            }
+            else
+            {
+                Console.WriteLine("Out of ability");
+            }
+        }
+    }
+}
---
+++ Ex.4.1/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Ex4.1 - thiết kế menu
+using System;
+namespace Ex4_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            do{
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Draw the triangle");
+                Console.WriteLine("2. Draw the square");
+                Console.WriteLine("3. Draw the rectangle");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Enter your choice: ");
+                choice = Int32.Parse(Console.ReadLine());
+                switch(choice)
+                {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    DrawTriangle();
+                    break;
+                case 2:
+                    DrawSquare();
+                    break;
+                case 3:
+                    DrawRectangle();
+                    break;
+                    }
+            }while(choice != 0);
+        }
+        static void DrawTriangle()
+        {
+            Console.WriteLine("Draw the triangle");
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void DrawSquare()
+        {
+            Console.WriteLine("Draw the square");
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 7; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void DrawRectangle()
+        {
+            Console.WriteLine("Draw the rectangle");
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 7; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
